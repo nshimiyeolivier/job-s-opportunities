ca9ad163cffbb18062d0965404c3971ad95c1d05
oliviernchima <nchima2010@gmail.com>
2018-12-19 22:36:27 +0100

    ok

--- assets/custom.js
+++ assets/custom.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
             $('.jquery').animate({width:'96%'},2000);
         		$('.php').animate({width:'85%'},2000);
         		$('.wp').animate({width:'90%'},2000);
-            $('.angular').animate({width:'90%'},2000);
+            $('.angular').animate({width:'80%'},2000);
             $('.git').animate({width:'95%'},2000);
             $('.xd').animate({width:'97%'},2000);
             $('.photoshop').animate({width:'88%'},2000);
